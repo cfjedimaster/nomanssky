d6f679c273430bd7fe29b400a08e198a0c58e21d
Raymond Camden <raymondcamden@gmail.com>
2017-08-30 16:47:49 -0500

    more work

--- client/app.js
+++ client/app.js
@@ -17,15 +17,40 @@ let bpApp = new Vue({
 			let thatFilter = this.filter.toLowerCase();
 			return this.blueprints.filter(function(bp) {
 				if(thatFilter === '') return true;
-				if(bp.title.toLowerCase().indexOf(thatFilter) >= 0) return true;
+				if(bp.name.toLowerCase().indexOf(thatFilter) >= 0) return true;
 				return false;
 			});
 		},
 		neededResources:function() {
-			let needed = [];
+			let needed = {};
+			let result = [];
 			/*
 			ok, so our shopping cart (items) has an array of items and requirements.
 			*/
+			for(let x=0;x<this.items.length;x++) {
+				let item = this.items[x];
+				console.log('doing '+JSON.stringify(item));
+				//first, find it
+				for(let i=0;i<this.blueprints.length;i++) {
+					if(this.blueprints[i].name === item.name) {
+						console.log('i need to add raw for '+item.name, this.blueprints[i].rawresources);
+						for(let key in this.blueprints[i].rawresources) {
+							if(!needed[key]) needed[key] = 0;
+							needed[key] += Number(this.blueprints[i].rawresources[key]) * item.qty;
+						}
+					}
+				}
+			}
+			//now convert it to an array
+			for(let key in needed) {
+				result.push({name:key, qty: needed[key]});
+			}
+			result.sort(function(a,b) {
+				if(a.name > b.name) return 1;
+				if(a.name < b.name) return -1;
+				return 0;
+			});
+			return result;
 		}
 	},
 	methods:{
@@ -38,17 +63,17 @@ let bpApp = new Vue({
 			*/
 			let existing = -1;
 			for(let i=0;i<this.items.length;i++) {
-				if(this.items[i].title === item) existing = i;
+				if(this.items[i].name === item) existing = i;
 			}
 			if(existing === -1) {
-				this.items.push({title:item, qty:0});
+				this.items.push({name:item, qty:1});
 			} else {
 				this.items[existing].qty++;
 			}
 
 			this.items = this.items.sort((a,b) => {
-				if(a.title > b.title) return 1;
-				if(a.title < b.title) return -1;
+				if(a.name > b.name) return 1;
+				if(a.name < b.name) return -1;
 				return 0;
 			});
 		},
@@ -56,7 +81,7 @@ let bpApp = new Vue({
 			console.log('remove '+item);
 			let existing = -1;
 			for(let i=0;i<this.items.length;i++) {
-				if(this.items[i].title === item) existing = i;
+				if(this.items[i].name === item) existing = i;
 			}
 			if(existing !== -1) {
 				//in theory it should ALWAYs match, but...
