9e8bf2b7c26156d603b3a1b76b37d0789aa199de
Raymond Camden <raymondcamden@gmail.com>
2017-10-12 10:07:24 -0500

    sw support

--- client/app.js
+++ client/app.js
@@ -87,3 +87,17 @@ let bpApp = new Vue({
 		}
 	}
 });
+
+document.addEventListener('DOMContentLoaded', init, false);
+function init() {
+
+	if("serviceWorker" in navigator) {
+		navigator.serviceWorker.register('serviceworker.js')
+		.then((registration) => {
+			console.log('Service Worker installed!');
+		}).catch((err) => {
+			console.error('Service Worker failed', err);
+		});
+	}
+
+}
---
+++ client/serviceworker.js
@@ -0,0 +1,52 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+	'https://cfjedimaster.github.io/nomanssky/client/index.html',
+	'https://cfjedimaster.github.io/nomanssky/client/index.html?utm_source=homescreen',
+  'https://cfjedimaster.github.io/nomanssky/client/app.css',
+  'https://cfjedimaster.github.io/nomanssky/client/data.json',
+  'https://cfjedimaster.github.io/nomanssky/client/app.css',
+  'https://cfjedimaster.github.io/nomanssky/client/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+	// Perform install steps
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+		.then(function(cache) {
+			console.log('Opened cache');
+			return cache.addAll(urlsToCache);
+		})
+		.catch(function(e) {
+			console.log('Error from caches open', e);
+		})
+	)
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+	  caches.match(event.request)
+		.then(function(response) {
+		  // Cache hit - return response
+		  if (response) {
+				console.log('got it from cache', event.request);
+				return response;
+		  }
+		  return fetch(event.request);
+		}
+	  )
+	);
+  });
+  
+self.addEventListener("activate", function(event) {  
+	event.waitUntil(
+		caches.keys().then(function(cacheNames) {
+	    	return Promise.all(
+				cacheNames.map(function(cacheName) {          
+					if (CACHE_NAME !== cacheName) {
+						return caches.delete(cacheName);          
+					}        
+				})      
+			);    
+		})  
+	);
+});
